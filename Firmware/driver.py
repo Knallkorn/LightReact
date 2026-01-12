214935967a372bd9a1eb2816f115adac74c40419
Knallkorn <Flynn@rewill.net>
2026-01-13 00:35:48 +1100

    Cleaned Up Code
    Cleaned up code and reduced sleep time

--- Firmware/driver.py
+++ Firmware/driver.py
@@ -1,41 +1,41 @@
+# Imports
 import serial
 from time import sleep
 import numpy
 from matplotlib.colors import hsv_to_rgb
 
+# Initialise global variables
 numLeds = 140
-numRGB = numLeds * 3
 
 startMarker = 0x0F
 endMarker = 0xF0
 
 hue = 0
 
-link = serial.Serial(port='COM6', baudrate=115200, timeout=1)
-sleep(2)
-
 colors = numpy.full(shape=(numLeds, 3), fill_value=[0.0, 1.0, 1.0])
 
 denormalise = numpy.vectorize((lambda x: int(x*255)))
 
+# Initialise link
+link = serial.Serial(port='COM6', baudrate=115200, timeout=1)
+sleep(2)
+
+# Main loop
 while True:
     
+    # TEMP - Rainbow loop
     hue += 1
-    if hue == 256:
+    if hue >= 256:
         hue = 0
     colors[:, 0] = numpy.full(shape=(numLeds), fill_value=hue/255)
 
+    # Package data
     data = denormalise(hsv_to_rgb(colors).flatten()).astype(numpy.uint8)
     data = numpy.insert(data, 0, startMarker)
     data = numpy.append(data, endMarker)
     data = data.astype(dtype=numpy.uint8)
 
+    # Send data
     packet = data.tobytes()
     link.write(packet)
-    sleep(0.05)
-    recData = link.read_all()
-    #print(f"Sent: {packet}\n")
-    #print(f"Recieved: {recData}")
-    #print(f"Size: {len(leds.tolist())}")
-    #print(f"Matching: {recData == leds.tobytes()}")
-    print("Loop")
+    sleep(0.01)
--- Firmware/main/main.ino
+++ Firmware/main/main.ino
@@ -1,3 +1,4 @@
+// Includes and macros
 #include <FastLED.h>
 
 #define NUM_LEDS 140
@@ -8,6 +9,7 @@
 #define START_MARKER 0x0F // 00001111
 #define END_MARKER 0xF0 // 11110000
 
+// Initalise global variables
 const size_t dataLength = NUM_RGB;
 uint8_t data[dataLength];
 bool newData = false;
@@ -15,20 +17,19 @@ bool newData = false;
 CRGB leds [NUM_LEDS];
 
 void setup() {
+  // Initialise serial
   Serial.begin(115200);
   Serial.setTimeout(1);
 
+  // Relay control
   pinMode(SIGNAL_PIN, OUTPUT);
   digitalWrite(SIGNAL_PIN, HIGH);
-  delay(100);
+  delay(100); // Delay to ensure LEDs are booted before data sent
   
+  // Initialise LEDs
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds,NUM_LEDS);
   FastLED.clear();
-  FastLED.setBrightness(50);
-  for (int i=0; i<NUM_LEDS; i++) {
-    leds[i].setHSV(0, 255, 0);
-  }
-  FastLED.show();
+  FastLED.setBrightness(50); // Currently only static brightness at 50
 }
 
 void loop() {
@@ -37,34 +38,33 @@ void loop() {
 }
 
 void rcxData() {
-  static bool rcxCur = false;
-  static uint16_t ndx = 0;
-  uint8_t rcx;
+  static bool rcxCur = false; // Whether currently recieving
+  static uint16_t ndx = 0; // Data index iterator
+  uint8_t rcx; // Buffer to hold serial input
 
   while (Serial.available() > 0 && newData == false) {
-    rcx = Serial.read();
+    rcx = Serial.read(); // Assuming that processing speed is considerably higher than baud, so only one byte to process at a time
 
     if (rcxCur == true) {
-      if (rcx != END_MARKER) {
+      if (rcx != END_MARKER) { // Payload handling
         data[ndx] = rcx;
         ndx++;
         if (ndx >= dataLength) {
           ndx = dataLength - 1;
         }
-      } else {
+      } else { // End of packet
         rcxCur = false;
         ndx = 0;
         newData = true;
       }
-    } else if (rcx == START_MARKER) {
+    } else if (rcx == START_MARKER) { // Begin packet
       rcxCur = true;
     }
   }
 }
 
 void echoData() {
-  if (newData == true) {
-    //Serial.write(data, dataLength);
+  if (newData == true) { // Only update LEDs if there a new packet has been processed
     for (int i=0; i<NUM_LEDS; i++) {
       leds[i].setRGB(data[(i*3)],data[(i*3)+1],data[(i*3)+2]);
     }
