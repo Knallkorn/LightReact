8ba883e40d812afedcb1c79e9e44f7ac9c73a691
Knallkorn <Flynn@rewill.net>
2026-01-12 15:23:49 +1100

    Reworked Serial
    Now using straight serial instead of SerialTransfer to reduce overhead

--- Firmware/driver.py
+++ Firmware/driver.py
@@ -1,38 +1,26 @@
-from pySerialTransfer import pySerialTransfer as txfer
+import serial
 import time
 import numpy
 
 numLeds = 140
 numRGB = numLeds * 3
-leds = numpy.arange(numRGB, dtype=numpy.uint16) #numpy.full(shape=numRGB, fill_value=255, dtype=numpy.uint8)
-leds = numpy.array_split(leds, (numRGB // 64)+1)
+dataLen = numRGB if numRGB % 64 == 0 else numRGB + (64-(numRGB%64))
 
-try:
-    link = txfer.SerialTransfer('COM6', baud=115200)
+link = serial.Serial(port='COM6', baudrate=115200, timeout=1)
+time.sleep(2)
 
-    link.open()
-    time.sleep(2)
+leds = numpy.full(shape=(numLeds), fill_value=255, dtype=numpy.uint8)
+leds = leds.flatten()
+split = list()
+for i in range(numLeds // 64):
+    split.append(64+(64*(i)))
+leds = numpy.split(leds, split)
 
-    for i in range(len(leds)):
-        ledList = leds[i].tolist()
-        listSize = link.tx_obj(ledList)
-        link.send(listSize, packet_id=i)
-
-        while not link.available():
-            if link.status.value < 0:
-                print('ERROR: {}'.format(link.status))
-        
-        dataBack = link.rx_obj(obj_type=type(ledList), obj_byte_size=listSize, list_format='i')
-        print(f"Recieved back: {dataBack}")
-
-except KeyboardInterrupt:
-    try:
-        link.close()
-    except:
-        pass
-except Exception as e:
-    print(e)
-    try:
-        link.close()
-    except:
-        pass
+for packArr in leds:
+    packData = packArr.tobytes()
+    print(f"Sent: {packData}")
+    link.write(packData)
+    time.sleep(0.1)
+    recData = link.read_all()
+    print(f"Recieved: {recData}")
+    print(f"Matching: {recData == packData}")
--- Firmware/main/main.ino
+++ Firmware/main/main.ino
@@ -1,23 +1,21 @@
 #include <FastLED.h>
-#include "SerialTransfer.h"
 
 #define NUM_LEDS 140
+#define NUM_RGB NUM_LEDS*3
 #define LED_PIN 4
 #define SIGNAL_PIN 8
 #define DELAY 2
 
-SerialTransfer txfer;
-
-const size_t dataLength = NUM_LEDS*3;
+const size_t dataLength = NUM_RGB;
 uint8_t data[dataLength];
-uint8_t packet = 0;
 
 int time;
 
 CRGB leds [NUM_LEDS];
+
 void setup() {
   Serial.begin(115200);
-  txfer.begin(Serial);
+  Serial.setTimeout(1);
   
   /*
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds,NUM_LEDS);
@@ -35,17 +33,10 @@ void setup() {
 }
 
 void loop() {
-  if (txfer.available()) {
-    for (int32_t i=0; i < txfer.bytesRead; i++)
-      txfer.packet.txBuff[i] = txfer.packet.rxBuff[i];
-
-    txfer.sendData(txfer.bytesRead);
-
-    uint8_t packLen = txfer.packet.txBuff[3]; // Get number of payload bytes
-
-    if (txfer.packet.txBuff[1] == 0 && packet != 0) { // Check packet ID (held in second byte)
-      packet = 0;
-    } else packet++;
+  if (Serial.available() > 0) {
+    byte packData[64];
+    int packLen = Serial.readBytes(packData, Serial.available());
+    Serial.write(packData, packLen);
   }
 
   /*
