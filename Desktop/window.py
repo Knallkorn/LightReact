4d92cb617e9e9abeabf094e837dc40446711b982
Knallkorn <43976660+Knallkorn@users.noreply.github.com>
2026-01-15 15:01:01 +1100

    Added Number of LEDs button

--- Desktop/window.py
+++ Desktop/window.py
@@ -4,18 +4,44 @@ class MainWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
+        self.numLeds = 140
+
         self.layout = QtWidgets.QVBoxLayout(self)
 
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.button.clicked.connect(self.func)
+        self.colorButton = QtWidgets.QPushButton("Color Picker")
+        self.colorButton.clicked.connect(self.openPicker)
 
-        self.text = QtWidgets.QLabel("No Colour")
+        self.colorText = QtWidgets.QLabel("No Colour")
 
-        self.layout.addWidget(self.button)
-        self.layout.addWidget(self.text)
-        
+        self.ledDownButton = QtWidgets.QPushButton("<")
+        self.ledDownButton.clicked.connect(self.ledDown)
+
+        self.ledUpButton = QtWidgets.QPushButton(">")
+        self.ledUpButton.clicked.connect(self.ledUp)
+
+        self.numLedsText = QtWidgets.QLabel(f"{self.numLeds}")
+
+        self.numLedsLayout = QtWidgets.QHBoxLayout()
+        self.numLedsLayout.addWidget(self.ledDownButton)
+        self.numLedsLayout.addWidget(self.numLedsText)
+        self.numLedsLayout.addWidget(self.ledUpButton)
+
+        self.layout.addWidget(self.colorButton)
+        self.layout.addWidget(self.colorText)
+        self.layout.addLayout(self.numLedsLayout)
+
     @QtCore.Slot()
-    def func(self):
+    def openPicker(self):
         color = QtWidgets.QColorDialog.getColor()
         if color.isValid():
-            self.text.setText(f"Selected Color: {color.name()}")
+            self.colorText.setText(f"Selected Color: {color.name()}")
+    
+    @QtCore.Slot()
+    def ledDown(self):
+        self.numLeds -= 1
+        self.numLedsText.setText(f"{self.numLeds}")
+    
+    @QtCore.Slot()
+    def ledUp(self):
+        self.numLeds += 1
+        self.numLedsText.setText(f"{self.numLeds}")
