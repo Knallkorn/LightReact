c817b64de6008cdbbd2cc5cf323851ced9cb4b72
Knallkorn <Flynn@rewill.net>
2026-01-17 01:47:46 +1100

    Added interpolation between colors

--- Firmware/driver.py
+++ Firmware/driver.py
@@ -7,17 +7,25 @@ from PIL import ImageGrab, Image
 # Functions
 def sampleScreen(width):
     img = ImageGrab.grab()
-    img = img.resize((width,round(img.size[1]/3)), resample=Image.Resampling.BOX)
+    img = img.resize((width,round(img.size[1]/5)), resample=Image.Resampling.BOX)
     size = img.size
     imgArr = numpy.array(img.get_flattened_data(), dtype=numpy.uint8).reshape((size[1],size[0],3))
     return numpy.floor(imgArr.mean(axis=0)).astype(numpy.uint8)
+
+def lerpColor(color1, color2, a):
+    if not (0 <= a <= 1):
+        a = max(0, min(1, a))
+    lerped = [int(c1*(1 - a) + c2*a) for c1, c2 in zip(color1, color2)]
+    return tuple(lerped)
 
 # Initialise global variables
 numLeds = 140
 
 startMarker = 0x1
 
-colors = numpy.full(shape=(numLeds, 3), fill_value=0, dtype=numpy.uint8)
+doLerp = True
+
+colors = sampleScreen(numLeds)
 
 # Initialise link
 link = serial.Serial(port='COM6', baudrate=115200, timeout=1)
@@ -26,7 +34,12 @@ sleep(2)
 # Main loop
 while True:
     
-    colors = sampleScreen(numLeds)
+    targetCol = sampleScreen(numLeds)
+    if doLerp == True:
+        for i in range(0, numLeds):
+            colors[i] = lerpColor(colors[i], targetCol[i], 0.2)
+    else:
+        colors = sampleScreen(numLeds)
     colors[colors == 1] = 0
 
     # Package data
@@ -37,5 +50,3 @@ while True:
     # Send data
     packet = data.tobytes()
     link.write(packet)
-    # Sleep commented out for now as sampling screen should provide enough time. If moved to another thread, reimplement
-    sleep(0.01)
